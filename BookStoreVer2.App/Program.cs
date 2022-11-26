f7e7703284164d18ca7a9df99652490eed86ec78
Feligena <kiskatakizka@gmail.com>
2022-11-26 16:39:16 +0500

    Добавлена зависимость DbConfig, создан файл db.json, переписан метод OnConfiguring. В Program.cs создается экземпляр класса DbBookStore. Приложение запускается без ошибок

---
+++ BookStoreVer2.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreVer2.Lib;
+
+namespace BookStoreVer2.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DbBookStore();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
